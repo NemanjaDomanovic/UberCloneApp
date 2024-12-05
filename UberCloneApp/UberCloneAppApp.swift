ae7ab89132884a601a8de6444b7df179a23dc9da
Nemanja Domanovic <domanovic.nemanja@gmail.com>
2024-12-05 09:04:11 +0100

    Initial Commit

---
+++ UberCloneApp/UberCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneAppApp.swift
+//  UberCloneApp
+//
+//  Created by Nemanja Domanovic on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
