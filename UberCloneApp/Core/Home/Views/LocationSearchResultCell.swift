85fb08394c450ca2f98303a416020cd787f21b31
Nemanja Domanovic <domanovic.nemanja@gmail.com>
2024-12-05 20:02:50 +0100

    Dodata search funkcionalnost, kao i dugme za vraćanje unazad.

---
+++ UberCloneApp/Core/Home/Views/LocationSearchResultCell.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchResultCell.swift
+//  UberCloneApp
+//
+//  Created by Nemanja Domanovic on 12/5/24.
+//
+
+import SwiftUI
+
+struct LocationSearchResultCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationSearchResultCell()
+}
---
+++ UberCloneApp/Core/LocationSearch/Views/LocationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchView.swift
+//  UberCloneApp
+//
+//  Created by Nemanja Domanovic on 12/5/24.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationSearchView()
+}
